305f4f490b03595c566d719ca4a5c8a36448e44c
Collins Kariuki <cmka2020@mymail.pomona.edu>
2024-05-22 11:41:30 -0700

    Add comments to explain the learned concepts

--- Mini-Projects/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Mini-Projects/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -44,8 +44,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+// Compose uses a Modifier object, which is a collection of elements that decorate or modify the
+// behavior of Compose UI elements. You use this to style the UI components of the Dice Roller app's
+// components.
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf( 1) }
+    // NB (1): Composables are stateless by default, which means that they don't hold a value and
+    // can be recomposed any time by the system, which results in the value being reset. However,
+    // Compose provides a convenient way to avoid this. Composable functions can store an object in
+    // memory using the remember composable.
+    // NB (2): The mutableStateOf() function returns an observable which basically means that when
+    // the value of the result variable changes, a recomposition is triggered, the value of the
+    // result is reflected, and the UI refreshes.
+    var result by remember { mutableIntStateOf( 1) }
+
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -54,12 +65,27 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
+    Column(modifier = modifier,
+        // centers the image and the button
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
         Image(painter = painterResource(imageResource), contentDescription = result.toString())
 
+        Spacer(modifier = Modifier.height(16.dp))
+
         Button(
+            // NB (1): The braces, in this case, represent what is known as a lambda, the area
+            // inside of the braces being the lambda body. When a function is passed as an argument,
+            // it can also be referred to as a "callback".
+            // NB (2): A lambda is a function literal, which is a function like any other, but
+            // instead of being declared separately with the fun keyword, it is written inline and
+            // passed as an expression.
+
+            // randomly generates a number between 1 and 6 and assigns it to the result variable
             onClick = { result = (1..6).random() },
         ) {
+            // Text that is displayed on the button
             Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
     }
@@ -69,7 +95,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 @Composable
 fun DiceRollerApp() {
     DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+        .fillMaxSize() // So the layout fills the entire screen.
+        .wrapContentSize(Alignment.Center) // So the text is centered vertically and horizontally.
     )
 }
